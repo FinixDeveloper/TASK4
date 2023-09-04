6bac6b5b5999fdfe2a8a5809ef8bdcef2a198b47
Praveen Kumar <142767317+FinixDeveloper@users.noreply.github.com>
2023-09-04 20:00:56 +0530

    complete.js

--- complete.js
+++ complete.js
@@ -192,3 +192,104 @@ palindromes = (arr) => {
   palindromes(value);
 
 
+//f.Return median of two sorted arrays of same size.
+
+//Anonymous :
+
+let arr1 = [1, 12, 15, 26, 38,24];
+let arr2 = [2, 13, 17, 30, 45, 47];
+let value = function(a, b) {
+
+  let c = [...a, ...b].sort((a, b) => a - b);
+
+  const half = c.length / 2 | 0;
+  
+if (c.length % 2) {
+  return c[half];
+} else {
+  
+  return (c[half] + c[half - 1]) / 2;
+}
+}
+console.log(value(arr1, arr2));
+
+//IIFE:
+
+    ( function() {
+  let arr1 = [1, 12, 15, 26, 38,24];
+  let arr2 = [2, 13, 17, 30, 45, 47];
+  
+    let c = [...arr1, ...arr2].sort((arr1, arr2) => arr1 - arr2);
+  
+    const half = c.length / 2 | 0;
+    
+  if (c.length % 2) {
+  console.log(c[half]);
+  } else {
+    
+  console.log((c[half] + c[half - 1]) / 2);
+  }
+  
+  })()
+
+//g. Remove duplicates from an 
+
+//Anonymous :
+
+let findDuplicates = function () {
+  
+  const yourArray = [1, 1, 2, 3, 4, 5, 5]
+  let duplicates = []
+  const tempArray = [...yourArray].sort() 
+  for (let i = 0; i < tempArray.length; i++) {
+    if (tempArray[i + 1] === tempArray[i]) {
+      duplicates.push(tempArray[i])
+    }
+  }
+  console.log(duplicates) 
+}
+findDuplicates()
+
+//IIFE :
+
+
+(function () {
+  
+  let numbers = [1, 2, 3, 2, 4, 5, 5, 6];
+  
+  let duplicates = numbers.filter((item, index) => index !== numbers.indexOf(item));
+  
+  console.log(duplicates);
+})()
+
+
+//h.Remove duplicates from an array
+
+//Anonymous :
+
+
+let rotateArray =function(A, K) {
+  if (A.length === K || K === 0) {
+      return A;
+  }
+  K = K % A.length;
+  for (let i = 0; i < K; i++) {
+      A.unshift(A.pop());
+  }
+console.log(A)
+}
+rotateArray([1,2,3,4,5],2)
+
+//IIFE :
+
+(function(A, K) {
+  if (A.length === K || K === 0) {
+      return A;
+  }
+  K = K % A.length;
+  for (let i = 0; i < K; i++) {
+      A.unshift(A.pop());
+  }
+console.log(A)
+})([1,2,3,4,5],2)
+
